16f3ee0ec4c46d97073765406b8c3efa2b348aaf
Obaloluwa Olajide <obaloluwao008@gmail.com>
2025-08-12 20:58:10 +0100

    Delete wk.py

--- wk.py
+++
@@ -1,9 +0,0 @@
-my_list = []
-my_list.append(10)
-my_list.append(20)
-my_list.append(30)
-my_list.append(40)
-
-my_list.insert(2, 15)
-print(my_list )  # Output: [10, 20, 15, 30, 40]
-my_list.remove(20)
